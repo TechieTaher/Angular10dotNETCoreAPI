d38ed6d5f8cce171f5ace361453d3a94afcd70d0
Techie Taher <33249157+TechieTaher@users.noreply.github.com>
2022-08-16 22:21:01 +0530

    comment photos directory path

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -65,12 +65,12 @@ namespace WebAPI
             });
 
 
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(
-                    Path.Combine(Directory.GetCurrentDirectory(),"Photos")),
-                RequestPath="/Photos"
-            });
+            //app.UseStaticFiles(new StaticFileOptions
+            //{
+              //  FileProvider = new PhysicalFileProvider(
+                //    Path.Combine(Directory.GetCurrentDirectory(),"Photos")),
+                //RequestPath="/Photos"
+            //});
         }
     }
 }
